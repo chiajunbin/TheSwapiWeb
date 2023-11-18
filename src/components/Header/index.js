d27d90f673e7df1af172b3b82ce1fee8213ee34e
chiajunbin <chiajunbin007@gmail.com>
2023-11-18 16:12:49 +0800

    [IMP] second-try

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,28 +2,29 @@ import React from "react";
 import { Row, Col } from "reactstrap";
 
 const Header = () => {
-
-	const logoStyle = {
-    width: '50px', 
-    height: 'auto', 
-    marginRight: '10px',
-		marginLeft: '30px',
+  const logoStyle = {
+    width: "50px",
+    height: "auto",
+    marginRight: "10px",
+    marginLeft: "30px",
   };
 
-	const headerStyle = {
-    boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)', 
+  const headerStyle = {
+    boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
   };
 
   return (
-
-      <header style={headerStyle}>
-        <Row className="align-items-center">
-          <Col xs="auto">
-            <img src={process.env.PUBLIC_URL + '/star-wars-logo.png'} alt="Logo" style={logoStyle} />
-          </Col>
-        </Row>
-      </header>
-
+    <header style={headerStyle}>
+      <Row className="align-items-center">
+        <Col xs="auto">
+          <img
+            src={process.env.PUBLIC_URL + "/star-wars-logo.png"}
+            alt="Logo"
+            style={logoStyle}
+          />
+        </Col>
+      </Row>
+    </header>
   );
 };
 
--- src/containers/Starship/index.js
+++ src/containers/Starship/index.js
@@ -1,100 +1,114 @@
-import { useEffect, useState } from 'react';
-import { Pagination, PaginationItem, PaginationLink, Container, Table } from 'reactstrap';
-import withDataFetch from './action';
-import { Spinner } from 'reactstrap'; 
-import Header from '../../components/Header';
+import { useEffect, useState } from "react";
+import {
+  Pagination,
+  PaginationItem,
+  PaginationLink,
+  Container,
+  Table,
+} from "reactstrap";
+import withDataFetch from "./action";
+import { Spinner } from "reactstrap";
+import Header from "../../components/Header";
 
 const DataTable = ({ data, getStarshipList, loading, totalPage }) => {
+  const [page, setPage] = useState(1);
 
-    const [page, setPage] = useState(1);
+  useEffect(() => {
+    getStarshipList(page);
+  }, [page]);
 
-    useEffect(()=>{
-      getStarshipList(page);
-    },[page])
-    
-    console.log(totalPage);
+  console.log(totalPage);
 
-    return(
+  return (
     <>
-    <div>
-      <Header/>
-    </div>
+      <div>
+        <Header />
+      </div>
 
-    <Container style={{ paddingTop: '30px' }}>
-      <Table hover>
-      
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Model</th>
-          <th>Class</th>
-          <th>Manufacturer</th>
-          <th>Length</th>
-          <th>Crew</th>
-          <th>Passenger</th>
-          <th>Cargo</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map(starship => (
-          <tr key={starship.name}>
-            <td>{starship.name}</td>
-            <td>{starship.model}</td>
-            <td>{starship.starship_class}</td>
-            <td>{starship.manufacturer}</td>
-            <td>{starship.length}</td>
-            <td>{starship.crew}</td>
-            <td>{starship.passengers}</td>
-            <td>{starship.cargo_capacity}</td>
-          </tr>
-        ))}
-      </tbody>
-      </Table>
+      <Container style={{ paddingTop: "30px" }}>
+        <Table hover>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Model</th>
+              <th>Class</th>
+              <th>Manufacturer</th>
+              <th>Length</th>
+              <th>Crew</th>
+              <th>Passenger</th>
+              <th>Cargo</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((starship) => (
+              <tr key={starship.name}>
+                <td>{starship.name}</td>
+                <td>{starship.model}</td>
+                <td>{starship.starship_class}</td>
+                <td>{starship.manufacturer}</td>
+                <td>{starship.length}</td>
+                <td>{starship.crew}</td>
+                <td>{starship.passengers}</td>
+                <td>{starship.cargo_capacity}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
 
         <Pagination>
-        <PaginationItem>
-        <PaginationLink
-          first
-        />
-
-        </PaginationItem>
-        <PaginationItem>
-        <PaginationLink
-          href="#"
-          previous
-        />
-        </PaginationItem>
-
-        {[...Array(totalPage)].map((x,i) =>
-          <PaginationItem active={page === (i + 1)}>
-            <PaginationLink onClick={() => {setPage(i + 1)}}>
-              {i + 1}
-            </PaginationLink>
+          <PaginationItem>
+            <PaginationLink first onClick={() => setPage(1)} />
           </PaginationItem>
-        )}
-        
-        <PaginationItem>
-        <PaginationLink
-          href="#"
-          next
-        />
-        </PaginationItem>
-        <PaginationItem>
-        <PaginationLink
-          href="#"
-          last
-        />
-        </PaginationItem>
-      </Pagination>
-    </Container>
-    
-    {console.log(loading)}
-    {loading && (
-        <div className="d-flex align-items-center justify-content-center" style={{ position: 'fixed',minWidth: '100%', minHeight: '100vh', backgroundColor: 'blue', zIndex:999, top:0, left:0}}>
-            <Spinner color="primary" />
+
+          <PaginationItem>
+            <PaginationLink
+              href="#"
+              previous
+              onClick={() => setPage(page - 1)}
+            />
+          </PaginationItem>
+
+          {[...Array(totalPage)].map((x, i) => (
+            <PaginationItem active={page === i + 1}>
+              <PaginationLink
+                onClick={() => {
+                  setPage(i + 1);
+                }}
+              >
+                {i + 1}
+              </PaginationLink>
+            </PaginationItem>
+          ))}
+
+          <PaginationItem>
+            <PaginationLink href="#" next onClick={() => setPage(page + 1)} />
+          </PaginationItem>
+
+          <PaginationItem>
+            <PaginationLink href="#" last onClick={() => setPage(totalPage)} />
+          </PaginationItem>
+        </Pagination>
+      </Container>
+
+      {console.log(loading)}
+      {loading && (
+        <div
+          className="d-flex align-items-center justify-content-center"
+          style={{
+            position: "fixed",
+            minWidth: "100%",
+            minHeight: "100vh",
+            backgroundColor: "rgba(255, 255, 255, 0.8)",
+            zIndex: 999,
+            top: 0,
+            left: 0,
+          }}
+        >
+          <Spinner color="primary" />
         </div>
-        )}
-    </>)
+      )}
+    </>
+  );
 };
 
-  export default withDataFetch(DataTable)
+export default withDataFetch(DataTable);
